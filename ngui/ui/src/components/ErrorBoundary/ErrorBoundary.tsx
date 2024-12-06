949a8a842f0cb64be1a9169de60e1640cff4b1f0
v-hx <146182370+v-hx@users.noreply.github.com>
2024-12-06 09:17:24 +0300

    OS-7831: Reimplemented error boundary (#1210)
    This is supposed to reset the error caught by the error boundary when
    navigating to a different location.
    https://datatrendstech.atlassian.net/browse/OS-7831

--- ngui/ui/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ ngui/ui/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,24 +1,40 @@
-import { Component } from "react";
+import { Component, PropsWithChildren } from "react";
+import { useLocation, type Location } from "react-router-dom";
 import SomethingWentWrong from "components/SomethingWentWrong";
 
-class ErrorBoundary extends Component {
-  static defaultProps = {
-    FallbackComponent: SomethingWentWrong
-  };
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
 
-  state = {
-    error: null,
-    info: null
-  };
+type ErrorBoundaryProps = PropsWithChildren<{
+  location: Location;
+}>;
 
-  componentDidCatch(error, info) {
-    this.setState({ error, info });
+class ErrorBoundaryInner extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  // Set the local state when the error is caught to display a fallback error component
+  static getDerivedStateFromError(): Partial<ErrorBoundaryState> {
+    return { hasError: true };
+  }
+
+  componentDidUpdate(prevProps: ErrorBoundaryProps) {
+    if (prevProps.location.key !== this.props.location.key) {
+      this.setState({ hasError: false });
+    }
   }
 
   render() {
-    const { FallbackComponent } = this.props;
-    return this.state.error === null ? this.props.children : <FallbackComponent />;
+    return this.state.hasError ? <SomethingWentWrong /> : this.props.children;
   }
 }
+
+const ErrorBoundary = ({ children }: PropsWithChildren) => {
+  const location = useLocation();
+  return <ErrorBoundaryInner location={location}>{children}</ErrorBoundaryInner>;
+};
 
 export default ErrorBoundary;
