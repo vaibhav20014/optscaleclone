c1a6c3e3fb7bd3e407e0c40ce47df3f173ab8612
ek-hystax <33006768+ek-hystax@users.noreply.github.com>
2024-12-25 16:26:23 +0400

    OS-2765. Populate default resource owner field for new pools
    Updated the logic to fetch eligible employees for the Default Resource Owner field during pool creation by using the parent pool ID

--- ngui/ui/src/components/forms/PoolForm/CreatePoolForm.tsx
+++ ngui/ui/src/components/forms/PoolForm/CreatePoolForm.tsx
@@ -2,22 +2,16 @@ import { Box } from "@mui/material";
 import { FormProvider, useForm } from "react-hook-form";
 import ButtonLoader from "components/ButtonLoader";
 import FormButtonsWrapper from "components/FormButtonsWrapper";
-import EmployeesService from "services/EmployeesService";
 import PoolsService from "services/PoolsService";
-import { isOrganizationManager, isPoolManager } from "utils/employees";
 import { NameField, LimitField, TypeSelector, AutoExtendCheckbox, OwnerSelector } from "./FormElements";
 import { CreatePoolFormProps, CreatePoolFormValues } from "./types";
 import { getCreateFormDefaultValues } from "./utils";
 
 const CreatePoolForm = ({ parentId, onSuccess, unallocatedLimit }: CreatePoolFormProps) => {
-  const { useCreatePool } = PoolsService();
+  const { useCreatePool, useGetPoolOwners } = PoolsService();
   const { isLoading: isCreatePoolLoading, createPool } = useCreatePool();
 
-  const { useGet: useGetEmployees } = EmployeesService();
-
-  const { isLoading: isGetEmployeesLoading, employees } = useGetEmployees();
-
-  const owners = employees.filter((employee) => isOrganizationManager(employee) || isPoolManager(employee, parentId));
+  const { poolOwners, isDataReady: isPoolOwnersDataReady } = useGetPoolOwners(parentId);
 
   const methods = useForm<CreatePoolFormValues>({
     defaultValues: getCreateFormDefaultValues()
@@ -32,7 +26,7 @@ const CreatePoolForm = ({ parentId, onSuccess, unallocatedLimit }: CreatePoolFor
       <form data-test-id="form_add_pool" onSubmit={onSubmit} noValidate>
         <NameField />
         <TypeSelector />
-        <OwnerSelector isLoading={isGetEmployeesLoading} owners={owners} helpMessageId="createPoolDefaultOwnerHelp" />
+        <OwnerSelector isLoading={!isPoolOwnersDataReady} owners={poolOwners} helpMessageId="createPoolDefaultOwnerHelp" />
         <LimitField unallocatedLimit={unallocatedLimit} />
         <AutoExtendCheckbox />
         <FormButtonsWrapper justifyContent="space-between">
